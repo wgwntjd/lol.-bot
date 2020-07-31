7778add4dd8cc6788848d7bea2150e52d3f1ca7d
wgwntjd <k22675427@gmail.com>
2020-07-31 11:26:19 +0900

    lol. bot

--- src/commands.py
+++ src/commands.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from .func_search import get_profile_embed, get_qu_embed
+from .func_search import get_profile_embed, get_qu_embed, invite_bot, get_help
 
 
 game = discord.Game("롤. 유호진 씨발련 어떻게 죽일지 생각 중")
@@ -13,15 +13,21 @@ async def on_ready():
 
 
 @bot.command()
-async def 살인(ctx):
-    await ctx.send("어떻게 유호진을 죽여드릴까요??")
+async def 봇초대(ctx):
+    await ctx.send(embed=invite_bot())
+
+
+@bot.command()
+async def help(ctx):
+    await ctx.send(embed=get_help())
 
 
 @bot.command()
 async def 프로필(ctx):  #game_result, champion_icon, user_KDA, user_rating, game_types
     msg_wait = await ctx.send("프로필 가져오는 중입니다...")
     try:
-        embed = get_profile_embed(" ".join(ctx.message.content.split()[1:]))
+        username = " ".join(ctx.message.content.split()[1:])
+        embed = get_profile_embed(username)
         await msg_wait.delete()
     except Exception as e:
         await msg_wait.delete()
@@ -29,6 +35,7 @@ async def 프로필(ctx):  #game_result, champion_icon, user_KDA, user_rating, g
         print(e)
         return
     await ctx.send(embed=embed)
+
 
 @bot.command()
 async def 전적(ctx):
--- src/func_search.py
+++ src/func_search.py
@@ -2,13 +2,34 @@ import urllib
 import discord
 from .lib.search_user import SearchUser
 
+def invite_bot():
+    client_id = 737977514324000858
+    git_hub_url = 'https://github.com/wgwntjd/lol.-bot'
+    github_icon = 'https://cdn.icon-icons.com/icons2/844/PNG/512/Github_icon-icons.com_67091.png'
+    embed = discord.Embed(title="롤점하나 봇을 다른 서버에도 추가하고 싶다면 눌러주세요!", url="https://discord.com/oauth2/authorize?client_id={0}&scope=bot".format(client_id), color=0x0475b8)
+    embed.set_author(name='development github', url=git_hub_url, icon_url=github_icon)
+
+    return embed
+
+def get_help():
+    git_hub_url = 'https://github.com/wgwntjd/lol.-bot'
+    github_icon = 'https://cdn.icon-icons.com/icons2/844/PNG/512/Github_icon-icons.com_67091.png'
+    embed = discord.Embed(title="롤점하나 명령어 목록", color=0x0475b8)
+    embed.set_author(name='development github', url=git_hub_url, icon_url=github_icon)
+    embed.add_field(name='.프로필 (소환사명)', value="소환사의 프로필을 가져옵니다.", inline=False)
+    embed.add_field(name='.전적 (소환사명):(판수)', value="소환사의 전적을 가져옵니다.\n만약 판수를 기입하지 않는다면 기본값으로 5판의 전적을 가져옵니다.", inline=False)
+    embed.add_field(name='.봇초대', value="다른 서버에서 봇점하나 봇을 사용하고 싶다면\n링크를 클릭한뒤 봇을 초대할 서버를 선택해주세요.", inline=False)
+    embed.set_footer(text="깃허브 star은 개발자들에게 힘이 됩니다.")
+    
+    return embed
+
 def get_profile_embed(arg):
     username = urllib.parse.quote(arg.replace(' ', '+'))  #username parsing
     su = SearchUser(username)
 
     average_stats = su.get_average_stats()
 
-    embed=discord.Embed(title=su.get_user_name(), url="https://www.op.gg/summoner/userName=" + username, description=su.get_top_rank(), color=0x0475b8)
+    embed = discord.Embed(title=su.get_user_name(), url="https://www.op.gg/summoner/userName=" + username, description=su.get_top_rank(), color=0x0475b8)
     embed.set_author(name=su.get_user_level() + "레벨")
     embed.set_thumbnail(url=su.get_user_icon())
     embed.add_field(name='개인/2인 랭크', value=su.get_tier()[0], inline=True)
@@ -36,28 +57,23 @@ def get_qu_embed(arg):
     game_types = su.get_recent_types(list_length)
     game_rating = su.get_recent_rating(list_length)
 
-    qu_count, win_count, lose_count = 0, 0, 0
+    result_count = []
     qu_value, kda_value, type_value = '', '', ''
     type_dict = {"Normal": "일반", "Ranked": "개인/2인 랭크", "Flex": "자유 5:5 랭크",
-    "Special":"특별게임모드", "ARAM":"무작위 총력전", "URF":"우르프", "Tutorial":"튜토리얼", "Bot":"봇전"}
+    "Special": "특별게임모드", "ARAM": "무작위 총력전", "URF": "우르프", "Tutorial": "튜토리얼", "Bot": "봇전"}
+    result_dict = {"Victory": "승", "Defeat": "패", "Remake": "무"}
+    result_cnt_dict = {"Victory": 1, "Defeat": 0, "Remake": 2}
 
     embed = discord.Embed(title=su.get_user_name(), url="https://www.op.gg/summoner/userName=" + username, description ='개인/2인랭 자유랭크\n%s %s' %(su.get_tier()[0], su.get_tier()[1]), color=0x0475b8)
     embed.set_thumbnail(url=su.get_user_icon())
     for i in range(list_length):
-        if game_result[i] == 'Victory':
-            qu_value += '승리\t' + game_champ[i] + '\n'
-            win_count += 1
-        elif game_result[i] == 'Defeat':
-            qu_value += '패배\t' + game_champ[i] + '\n'
-            lose_count += 1
-        else:
-            qu_value += '무승\t' + game_champ[i] + '\n'            
+        qu_value += result_dict[game_result[i]] + '\t' + game_champ[i] + '\n'    
+        result_count.append(result_cnt_dict[game_result[i]]) 
         kda_value += game_KDA[i] + ' ▷' + game_rating[i] + '\n'          
         type_value += type_dict[game_types[i]] + '\n'
-        qu_count += 1
 
-    embed.add_field(name=str(qu_count) + '전 ', value=qu_value, inline=True)
-    embed.add_field(name=str(win_count) + '승 ' , value=kda_value, inline=True)
-    embed.add_field(name=str(lose_count) + '패', value=type_value, inline=True)
+    embed.add_field(name=str(list_length) + '전 ', value=qu_value, inline=True)
+    embed.add_field(name=str(result_count.count(1)) + '승 ' , value=kda_value, inline=True)
+    embed.add_field(name=str(result_count.count(0)) + '패', value=type_value, inline=True)
 
     return embed
